4f47a76555d6acaa6b635d29bf23da650325d24e
Idriss Balijon <i.balijon@gmail.com>
2024-10-25 08:47:43 +0200

    on going validation feedback

--- public/modal.js
+++ public/modal.js
@@ -37,6 +37,41 @@ if (formContact) {
                 'Content-Type': 'application/json',
             },
         });
-        console.log(await response.json());
     });
+
+    if (formRegister) {
+      formRegister.addEventListener('submit', async (e) => {
+          e.preventDefault();
+          const formDataInscription = new FormData(formRegister);
+          formDataInscription.set(
+              'newsletter',
+              formDataInscription.get('newsletter') ? true : false
+          );
+          formDataInscription.set(
+            'consent',
+            formDataInscription.get('consent') ? true : false
+        );
+          let data = JSON.stringify(Object.fromEntries(formDataInscription));
+          console.log(data); // pour transformer en json le form
+  
+          const response = await fetch('/', {
+              method: 'POST',
+              body: data,
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+          });
+          treatResponse(response);
+      });
+}
+}
+
+function treatResponse(jsonResponse)
+{
+  jsonResponse.forEach( item => {
+
+    const selector = `error-${item.errorName}`;
+    const errorElement = document.querySelector(selector);
+    errorElement.textContent = item.message;
+  })
 }
